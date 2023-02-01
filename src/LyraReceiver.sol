67f56481384f028ccf1b59f532f8f027eb896d08
Aniket Sharma <aniket965.as@gmail.com>
2023-02-01 22:11:58 +0530

    feat: lyra init

---
+++ src/LyraReceiver.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+
+interface ILyraDeposit {
+    function initiateDeposit(address beneficiary, uint256 amountQuote) external;
+}
+
+interface UniswapRouter {
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+}
+
+contract LyraReceiver {
+    using SafeERC20 for IERC20;
+    ILyraDeposit public lyraDepositAddress;
+    address public depositTokenAddress;
+    address public receiverContract;
+    UniswapRouter public uniswapRouter;
+    uint256 public transferCost;
+
+    constructor(
+        ILyraDeposit _lyraDepositAddress,
+        address _depositTokenAddress,
+        address _receiverContract,
+        uint256 _transferCost,
+        UniswapRouter _uniswapRouter
+    ) {
+        lyraDepositAddress = _lyraDepositAddress;
+        depositTokenAddress = _depositTokenAddress;
+        transferCost = _transferCost;
+        receiverContract = _receiverContract;
+        uniswapRouter = _uniswapRouter;
+    }
+
+    function swapAndDeposit(
+        address token,
+        address userAddress,
+        uint256 minAmountOut
+    ) external {
+        uint256 allowance = IERC20(token).allowance(
+            receiverContract,
+            address(this)
+        );
+        IERC20(token).approve(address(uniswapRouter), allowance);
+        address[] memory path = new address[](2);
+        path[0] = token;
+        path[1] = depositTokenAddress;
+        try
+            uniswapRouter.swapExactTokensForTokens(
+                allowance,
+                minAmountOut,
+                path,
+                address(this),
+                block.timestamp
+            )
+        returns (uint256[] memory amounts) {
+            uint256 depositAmount = amounts[1];
+            IERC20(depositTokenAddress).approve(
+                address(lyraDepositAddress),
+                depositAmount
+            );
+            lyraDepositAddress.initiateDeposit(userAddress, depositAmount);
+        } catch {
+            IERC20(token).transfer(userAddress, allowance);
+        }
+    }
+
+    function deposit(address token, address userAddress) external {
+        uint256 allowance = IERC20(token).allowance(
+            receiverContract,
+            address(this)
+        );
+        IERC20(depositTokenAddress).approve(
+            address(lyraDepositAddress),
+            allowance
+        );
+        try
+            lyraDepositAddress.initiateDeposit(userAddress, allowance)
+        {} catch {
+            IERC20(token).transfer(userAddress, allowance);
+        }
+    }
+}
